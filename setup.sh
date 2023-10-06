a68874efb38f2a31cf5d5afc820fcc4e3ba87b0a
myridaxdevelopment <147154559+myridaxdevelopment@users.noreply.github.com>
2023-10-06 13:15:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Update
+apt install
+apt upgrade
+sudo apt autoremove
+
 # Create a 24GB swap file
 sudo fallocate -l 24G /swapfile
 sudo chmod 600 /swapfile
@@ -11,24 +16,26 @@ sudo swapon /swapfile
 sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -m conntrack --ctstate NEW -m limit --limit 50/s -j ACCEPT
 sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -m conntrack --ctstate NEW -j DROP
 
-# Set a 40GB storage limit for newly created Docker containers
-echo '{"storage-opts": ["size=40G"]}' | sudo tee /etc/docker/daemon.json
-
-# Limit the network bandwidth of Docker containers to 50 Mbps (adjust as needed)
+# Remove Docker container bandwidth limits
+# To remove the bandwidth limits, you need to delete the qdisc for each container.
 # Replace 'CONTAINER_UUID' with the UUID of your Docker container
-docker exec CONTAINER_UUID tc qdisc add dev eth0 root tbf rate 50mbit burst 10kbit latency 50ms
+docker exec CONTAINER_UUID tc qdisc del dev eth0 root
+
+# Remove Docker container storage limits
+# You can remove the custom storage options from the Docker daemon configuration.
+sudo sed -i '/storage-opts/d' /etc/docker/daemon.json
+
+# Restart the Docker daemon to apply the changes
+sudo systemctl restart docker
 
 # Configure iptables rate limiting to protect against DDoS attacks (adjust as needed)
 # This example rate limits incoming traffic to 10 requests per second on port 80
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -m limit --limit 10/s -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j DROP
 
-# Restart the Docker daemon to apply the new configuration
-sudo systemctl restart docker
-
-# UFW Firewalls to allow 8443
-ufw enable
-ufw allow 8443
+# Enable UFW and allow port 8443
+sudo ufw enable
+sudo ufw allow 8443
 
 # Save the iptables rules and Docker daemon configuration to persist across reboots
 sudo apt-get install iptables-persistent -y
